f868ed78dda6709aee2b1e6c7a6f208f6fc98328
Thiéfaine <yaourtnut@gmail.com>
2016-03-30 11:32:02 +0200

    app.jsx page

--- frontend/react/components/containers/Containers.jsx
+++ frontend/react/components/containers/Containers.jsx
@@ -2,38 +2,55 @@ var React = require('react');
 import { Router, Route, Link, hashHistory } from 'react-router';
 
 var Containers = React.createClass({
-  getInitialState: function() {
-    return {
-      containersList: []
-    };
-  },
+    getInitialState: function() {
+        return {
+            containersList: []
+        };
+    },
   
-  componentDidMount: function() {
-    this.serverRequest = $.get("/containers.json", function (result) {
-      this.setState({
-        containersList: result
-      });
-    }.bind(this));
-  },
+    componentDidMount: function() {
+        this.serverRequest = $.get("/containers.json", function (result) {
+            this.setState({
+                containersList: result
+            });
+        }.bind(this));
+    },
 
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
+    componentWillUnmount: function() {
+        this.serverRequest.abort();
+    },
 
-  render: function() {
+render: function() {
     var results = this.state.containersList;
     return (
-      <div>
-        {results.map(function(result){
-          return (
-            <li key={result.id}>
-              <Link to={"/containers/"+result.id}>{result.name}</Link>
-            </li>
-          );
-        })}
-      </div>
+        <div className="containers">
+            <div className="row">
+                <div className="col-lg-12">
+                    <h1 className="page-header">Dashboard</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <ul className="nav" id="side-menu">
+                        {results.map(function(result){
+                            return (
+                                <li key={result.id}>
+
+                                    <Link to={"/containers/"+result.id}>
+                                         <i className="fa fa-file-o"></i>
+                                         {result.name}
+                                    </Link>
+                                    <br/>
+                                </li>
+
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
-  }
+}
 });
 
 module.exports = Containers;
