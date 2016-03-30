59c271fed8b0cd4cef5a2390c77c69afa8de681b
Thiéfaine <yaourtnut@gmail.com>
2016-03-30 15:13:32 +0200

    containers page

--- frontend/react/components/containers/Containers.jsx
+++ frontend/react/components/containers/Containers.jsx
@@ -20,37 +20,51 @@ var Containers = React.createClass({
         this.serverRequest.abort();
     },
 
-render: function() {
-    var results = this.state.containersList;
-    return (
-        <div className="containers">
-            <div className="row">
-                <div className="col-lg-12">
-                    <h1 className="page-header">Dashboard</h1>
+    render: function() {
+        var results = this.state.containersList;
+        return (
+            <div className="containers">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h1 className="page-header">Vos Containers</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <ul className="tags">
+                            {results.map(function(result){
+                                return (
+                                    <li key={result.id}>
+                                        <div className="triangle">
+                                            
+                                        </div>
+                                        <div className="contenu">
+                                            <div className="img">
+                                                <i className="fa fa-file-o"></i>
+                                            </div>
+                                            <div className="elem">
+                                                <div className="name">
+                                                    {result.name}
+                                                </div>
+                                                <div className="option">
+                                                    <Link to={"/containers/"+result.id}>
+                                                        <i className="fa fa-pencil"></i>
+                                                    </Link>
+                                                    <Link to={"/containers/"+result.id}>
+                                                        <i className="fa fa-trash-o"></i>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div> 
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-lg-12">
-                    <ul className="nav" id="side-menu">
-                        {results.map(function(result){
-                            return (
-                                <li key={result.id}>
-
-                                    <Link to={"/containers/"+result.id}>
-                                         <i className="fa fa-file-o"></i>
-                                         {result.name}
-                                    </Link>
-                                    <br/>
-                                </li>
-
-                            );
-                        })}
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
-}
+        );
+    }
 });
 
 module.exports = Containers;
