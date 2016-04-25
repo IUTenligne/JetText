da4c562820a77600759f870f2480c5ff5aff20fc
Pierre Duverneix <pierre.duverneix@gmail.com>
2016-04-25 20:56:28 +0200

    get block_id from cke-dialog

--- app/assets/javascripts/cke/plugins/uploader/dialogs/uploader.js
+++ app/assets/javascripts/cke/plugins/uploader/dialogs/uploader.js
@@ -10,31 +10,27 @@ CKEDITOR.dialog.add('uploaderDialog', function(editor) {
                 elements: [
                     {
                         type: 'html',
-                        html: '<form class="new_upload" id="new_upload" enctype="multipart/form-data" action="/uploads" accept-charset="UTF-8" data-remote="true" method="post"><input name="utf8" value="✓" type="hidden"><p><input class="uploader" name="upload[file]" id="upload_file" type="file"></p></form>'
+                        html: '<input name="upload[file]" id="cke_upload_file" type="file">'
                     }
                 ]
             }
         ],
         onOk: function() {
-            var dialog = this;
-            $('#new_upload').submit();
-            var file = $('#new_upload').find('#upload_file')[0].files[0];
-            var type = file.type.split('/')[0];
-            var url = currentUser.email + '/files/' + type + '/' + file.name;
-            if (file.type === 'image/png') {
-                var editor_elem = editor.document.createElement('img');
-                editor_elem.setAttribute('src', url);
-            } else if (file.type === 'application/pdf') {
-                var editor_elem = editor.document.createElement('a');
-                editor_elem.setAttribute('href', url);
-                editor_elem.appendText('PDF');
-            } else if (file.type === 'video/mp4' || file.type === 'application/force-download') {
-                var editor_elem = editor.document.createElement('video');
-                editor_elem.setAttribute('src', url);
-                editor_elem.setAttribute('controls', '');
-            }
+            var ed_name = editor.name.split("_");
+            var block_id = ed_name[ed_name.length - 1];
 
-            editor.insertElement(editor_elem);
+            var formData = new FormData();
+            formData.append("tempfile", $("#cke_upload_file")[0].files[0]);
+            formData.append("block_id", block_id);
+
+            $.ajax({
+                url: "/uploads",
+                type: "POST",
+                contentType: false,
+                cache: false,
+                processData: false,
+                data: formData
+            });
         }
     };
 });
