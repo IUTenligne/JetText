f8d5a4a40eb75b6d81f592348f54ba245ec76fc7
Pierre Duverneix <pierre.duverneix@gmail.com>
2016-06-04 19:16:55 +0200

    timeline

--- frontend/react/components/widgets/Timeline.jsx
+++ frontend/react/components/widgets/Timeline.jsx
@@ -7,7 +7,9 @@ var Timeline = React.createClass({
 	getInitialState: function() {
 	  return {
 	    blocks: [],
-	    currentPos: null
+	    currentPos: null,
+	    currentBlock: null,
+	    currentBlockId: null
 	  };
 	},
 
@@ -43,12 +45,28 @@ var Timeline = React.createClass({
 				maxScroll = Math.floor(height - offset),
 				currentPos = Math.floor( scroll / maxScroll * 100 );
 
+		var currentBlock = null;
+
+		for ( var i in this.props.blocks ) {
+			if (currentPos > 99) {
+				this.setState({ 
+					currentBlock: this.props.blocks[this.props.blocks.length-1],
+					currentBlockId: this.props.blocks.length
+				});
+			} else if ( scroll >= document.getElementById("block-" + this.props.blocks[i]["id"]).offsetTop ) {
+				this.setState({ 
+					currentBlock: this.props.blocks[i],
+					currentBlockId: parseInt(i) + 1
+				});
+			} 
+		}
+		console.log(currentPos);
     this.setState({ currentPos: currentPos });
 	},
 
 	handleClick: function(blockId, event) {
 		event.preventDefault();
-		var block = document.getElementById("block-"+blockId).offsetTop;
+		var block = document.getElementById("block-" + blockId).offsetTop;
 		window.scrollTo(0, block);
 	},
 
@@ -58,27 +76,33 @@ var Timeline = React.createClass({
 		var firstBlock = this.getFirst(blocks);
 		var lastBlock = this.getLast(blocks);
 
+		var progress = {
+			width: this.state.currentPos + "%"
+		};
+
 		return(
 			<div id="timeline">
-				<div class="timeline-container">
-					<div class="topic-timeline">
-						{firstBlock
-							? <a title={firstBlock.id} href="javascript:;" onClick={this.handleClick.bind(this, firstBlock.id)} class="widget-link start-link">Bloc 1</a>
+				<div className="timeline-container">
+					<div className="topic-timeline">
+						{ firstBlock
+							? <a title={firstBlock.id} href="javascript:;" onClick={this.handleClick.bind(this, firstBlock.id)} className="widget-link start-link">Bloc 1</a>
 							: null
 						}
-						<div class="timeline-scrollarea">
-							<div class="timeline-padding"></div>
-							<div class="timeline-scroller">
-								<div class="timeline-handle"></div>
-								<div class="timeline-scroller-content">
-									<div class="timeline-replies">1 / {total}</div>
-									<div class="timeline-ago">{this.state.currentPos ? this.state.currentPos : null}</div>
+						<div className="timeline-scrollarea">
+							<div className="timeline-padding"></div>
+							<div className="timeline-scroller">
+								<div className="timeline-handle"></div>
+								<div className="timeline-progress">
+									<div className="timeline-progress-content" style={progress}>
+										<div className="timeline-progress-content-inner">{ this.state.currentBlock ? this.state.currentBlockId + "/" + total : null }</div>
+									</div>
 								</div>
+								<div className="timeline-current">{ this.state.currentBlock ? this.state.currentBlockId : null }</div>
 							</div>
-							<div class="timeline-padding"></div>
+							<div className="timeline-padding"></div>
 						</div>
 						{ lastBlock
-							? <a title={lastBlock.id} href="javascript:;" onClick={this.handleClick.bind(this, lastBlock.id)} class="widget-link last-link">Bloc {blocks.length}</a>
+							? <a title={lastBlock.id} href="javascript:;" onClick={this.handleClick.bind(this, lastBlock.id)} className="widget-link last-link">Bloc {blocks.length}</a>
 							: null
 						}
 					</div>
