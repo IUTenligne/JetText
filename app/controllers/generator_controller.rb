45e4d510caef20e93d465af441687116d567c901
Thiéfaine <yaourtnut@gmail.com>
2016-06-28 12:08:22 +0200

    misc

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -253,30 +253,30 @@ class GeneratorController < ApplicationController
           end
         else
           if pages[i].name
-            content = content + "<li><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"#{i}-#{gsub_name(pages[i].name)}.html\">" + pages[i].name + "</a></li>\n"
+            content = content + "<li><a href=\"#{i}-#{gsub_name(pages[i].name)}.html\">" + pages[i].name + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
           else
-            content = content + "<li><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"#{i}\">" + i + "</a></li>\n"
+            content = content + "<li><a href=\"#{i}\">" + i + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
           end
         end
       else
         if index == true
           if pages[i].name
-            content = content + "<li><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"pages/#{pages[i].id}\">" + pages[i].name + "</a></li>\n"
+            content = content + "<li><a href=\"pages/#{pages[i].id}\">" + pages[i].name + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
           else
-            content = content + "<li><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"pages/#{pages[i].id}\">" + i + "</a></li>\n"
+            content = content + "<li><a href=\"pages/#{pages[i].id}\">" + i + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
           end
         else
           if current_page != nil && pages[i].id == current_page.id
             if pages[i].name
-              content = content + "<li class=\"active\"><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"#{pages[i].id}\">" + pages[i].name + "</a></li>\n"
+              content = content + "<li class=\"active\"><a href=\"#{pages[i].id}\">" + pages[i].name + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
             else
-              content = content + "<li class=\"active\"><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"#{pages[i].id}\">" + i + "</a></li>\n"
+              content = content + "<li class=\"active\"><a href=\"#{pages[i].id}\">" + i + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
             end
           else
             if pages[i].name
-              content = content + "<li><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"#{pages[i].id}\">" + pages[i].name + "</a></li>\n"
+              content = content + "<li><a href=\"#{pages[i].id}\">" + pages[i].name + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
             else
-              content = content + "<li><span class=\"level-icon\"><i class=\"fa\"></i></span><a href=\"#{pages[i].id}\">" + i + "</a></li>\n"
+              content = content + "<li><a href=\"#{pages[i].id}\">" + i + "</a><span class=\"level-icon\"><i class=\"fa\"></i></span></li>\n"
             end
           end
         end
